537cc8b86ef7863d66876d5813dbf59b0586c67b
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-12 16:29:01 +0530

    User model code added

--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,52 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  //
-});
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: "String",
+      required: [true, "Name is Required"],
+      minLength: [5, "Name must be at least 5 characters"],
+      maxLength: [50, "Name should be less than 50 characters"],
+      lowercase: true,
+      trim: true, // Removes unnecessary spaces
+    },
+    email: {
+      type: "String",
+      required: [true, "Email is Required"],
+      lowercase: true,
+      trim: true,
+      unique: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please fill in a valid email address",
+      ], // Matches email against regex
+    },
+    password: {
+      type: "String",
+      required: [true, "Password is Required"],
+      minLength: [8, "Password must be at least 8 characters"],
+      select: false, // Will not select password upon looking up a document
+    },
+    avatar: {
+      public_id: {
+        type: "String",
+      },
+      secure_url: {
+        type: "String",
+      },
+    },
+    role: {
+      type: "String",
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 

fc54ff42d714f700cbadcb97acb840a0ef3ff9a8
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-14 15:25:05 +0530

    File upload logic added via multer

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import AppError from "../utils/appError.js";
+import cloudinary from "cloudinary";
+import fs from "fs/promises";
 
 const cookieOptions = {
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
@@ -44,7 +46,32 @@ const register = async (req, res, next) => {
     );
   }
 
-  // TODO: File Upload
+  console.log("File Details", JSON.stringify(req.file));
+  // File Upload
+  // Run only if user sends a file
+  if (req.file) {
+    try {
+      const result = await cloudinary.v2.uploader.upload(req.file.path, {
+        folder: "lms", // Save files in a folder named lms
+        width: "250",
+        height: "250",
+        gravity: "faces", // This option tells cloudinary to center the image around detected faces (if any) after cropping or resizing the original image
+        crop: "fill",
+      });
+
+      // If success
+      if (result) {
+        // Set the public_id and secure_url in DB
+        user.avatar.public_id = result.public_id;
+        user.avatar.secure_url = result.secure_url;
+
+        // After successful upload remove the file from local storage
+        fs.rm(`uploads/${req.file.filename}`);
+      }
+    } catch (error) {
+      new AppError(error || "File not uploaded, please try again", 500);
+    }
+  }
 
   // Save the user object
   await user.save();

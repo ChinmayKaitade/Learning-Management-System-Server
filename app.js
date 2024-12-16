959ca779c97a9e2db03261016068d195ebb9959c
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-16 19:41:54 +0530

    urlencoded middleware added for parsing URL-encoded data from incoming requests

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 // Built-In
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded data from incoming requests
 
 // Third-Party
 app.use(

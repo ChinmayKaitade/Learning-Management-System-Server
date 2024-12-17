f05a94735db1dc3ed69ec1c8b006711284723574
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-17 16:27:21 +0530

    Basic controllers code added

---
+++ controllers/course.controller.js
@@ -0,0 +1,9 @@
+const getAllCourses = async (req, res, next) => {
+  //
+};
+
+const getLecturesByCourseId = async (req, res, next) => {
+  //
+};
+
+export { getAllCourses, getLecturesByCourseId };

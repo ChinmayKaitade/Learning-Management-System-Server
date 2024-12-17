91111dd5e4c919f3704538a79cb467decab47eab
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-17 16:17:25 +0530

    Course model code added

---
+++ models/course.model.js
@@ -0,0 +1,64 @@
+import { Schema, model } from "mongoose";
+
+const courseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is Required"],
+      minLength: [8, "Title must be at least 8 characters "],
+      maxLength: [60, "Title should be less than 60 characters"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is Required"],
+      minLength: [8, "Description must be at least 8 characters "],
+      maxLength: [200, "Description should be less than 200 characters"],
+    },
+    category: {
+      type: String,
+      required: [true, "Category is Required"],
+    },
+    thumbnail: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      secure_url: {
+        type: String,
+        required: true,
+      },
+    },
+    lectures: [
+      {
+        title: String,
+        description: String,
+        lectures: {
+          public_id: {
+            type: String,
+            required: true,
+          },
+          secure_url: {
+            type: String,
+            required: true,
+          },
+        },
+      },
+    ],
+    numbersOfLectures: {
+      type: Number,
+      default: 0,
+    },
+    createdBy: {
+      type: String,
+      required: [true, "Course Instructor name is Required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Course = model("Course", courseSchema);
+
+export default Course;

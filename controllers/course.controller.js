124bf847c48d4daee3321a9047ccdceff9a7b3c0
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-17 19:37:36 +0530

    Course controller getAllCourses logic added

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,5 +1,19 @@
+import Course from "../models/course.model.js";
+import AppError from "../utils/appError.js";
+
 const getAllCourses = async (req, res, next) => {
-  //
+  try {
+    // Find all the courses without lectures
+    const courses = await Course.find({}).select("-lectures");
+
+    res.status(200).json({
+      success: true,
+      message: "All Courses",
+      courses,
+    });
+  } catch (error) {
+    return next(new AppError("Error in fetching all Courses.", 400));
+  }
 };
 
 const getLecturesByCourseId = async (req, res, next) => {
--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { getAllCourses, getLecturesByCourseId } from "../controllers/course.controller.js";
+import {
+  getAllCourses,
+  getLecturesByCourseId,
+} from "../controllers/course.controller.js";
 
 const router = Router();
 
-router.get("/", getAllCourses);
-router.get("/:id", getLecturesByCourseId);
+// router.get("/", getAllCourses);
+// router.get("/:id", getLecturesByCourseId);
+
+router.route("/").get(getAllCourses);
+router.route("/:id").get(getLecturesByCourseId);
+
 
 export default router;

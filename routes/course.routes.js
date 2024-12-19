07a76463bf9ed8720ec294d098c60f759c43f856
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-19 14:55:57 +0530

    Authentication and Authorization middlewares added for course routes

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -6,8 +6,8 @@ import {
   removeCourse,
   updateCourse,
 } from "../controllers/course.controller.js";
-import { isLoggedIn } from "../middlewares/auth.middleware.js";
-import upload from '../middlewares/multer.middleware.js';
+import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
@@ -17,12 +17,17 @@ const router = Router();
 router
   .route("/")
   .get(getAllCourses)
-  .post(upload.single("thumbnail"), createCourse);
+  .post(
+    isLoggedIn,
+    authorizedRoles("ADMIN"),
+    upload.single("thumbnail"),
+    createCourse
+  );
 
 router
   .route("/:id")
   .get(isLoggedIn, getLecturesByCourseId)
-  .put(updateCourse)
-  .delete(removeCourse);
+  .put(isLoggedIn, authorizedRoles("ADMIN"), updateCourse)
+  .delete(isLoggedIn, authorizedRoles("ADMIN"), removeCourse);
 
 export default router;

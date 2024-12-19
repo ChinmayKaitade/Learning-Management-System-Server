dfa60c8136a98739b11b55392d616e58839fa4b8
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-19 19:19:29 +0530

    Payment controller getRazorpayApiKey logic added

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,5 +1,9 @@
 export const getRazorpayApiKey = async (req, res, next) => {
-  //
+  res.status(200).json({
+    success: true,
+    message: "Razorpay API Key.",
+    key: process.env.RAZORPAY_KEY_ID,
+  });
 };
 
 export const buySubscription = async (req, res, next) => {

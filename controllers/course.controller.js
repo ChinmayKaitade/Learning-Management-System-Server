2a1d691724ff100c3ab624f1ef5662877e4f4b69
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-01-04 16:01:16 +0530

    Add Course Lecture issue fixed

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,3 +1,4 @@
+import asyncHandler from "../middlewares/asyncHandler.middleware.js";
 import Course from "../models/course.model.js";
 import AppError from "../utils/appError.js";
 import cloudinary from "cloudinary";
@@ -157,73 +158,130 @@ const removeCourse = async (req, res, next) => {
   }
 };
 
-const addLecturesToCourseById = async (req, res, next) => {
-  try {
-    // Extracting title and description from the request body
-    const { title, description } = req.body;
+const addLecturesToCourseById = asyncHandler(async (req, res, next) => {
+  const { title, description } = req.body;
+  const { id } = req.params;
 
-    // Extracting id from the request parameters
-    const { id } = req.params;
+  let lectureData = {};
 
-    // If title description does not exists
-    if (!title || !description) {
-      return next(new AppError("Title and Description are Required.", 400));
-    }
-
-    // Finding the course via the course ID
-    const course = await Course.findById(id);
-
-    // If course does not found or does not exists
-    if (!course) {
-      return next(new AppError("Course with given id does not exist.", 404));
-    }
-
-    const lectureData = {
-      title,
-      description,
-      lectures: {},
-    };
-
-    // Run only if user sends a file
-    if (req.file) {
-      try {
-        const result = await cloudinary.v2.uploader.upload(req.file.path, {
-          folder: "lms",
-        });
-
-        // If success
-        if (result) {
-          // Set the public_id and secure_url in array
-          lectureData.lectures.public_id = result.public_id;
-          lectureData.lectures.secure_url = result.secure_url;
-        }
-
-        // After successful upload remove the file from local storage
-        fs.rm(`uploads/${req.file.filename}`);
-      } catch (error) {
-        // Send the error message
-        return next(new AppError(error.message, 500));
-      }
-    }
-
-    // Adding lecturesData in lectures
-    course.lectures.push(lectureData);
-
-    // number of lectures
-    course.numbersOfLectures = course.lectures.length;
-
-    // Save the course object
-    await course.save();
-
-    res.status(200).json({
-      success: true,
-      message: "Course Lecture added Successfully.",
-      course,
-    });
-  } catch (error) {
-    return next(new AppError(error.message, 500));
+  if (!title || !description) {
+    return next(new AppError("Title and Description are required", 400));
   }
-};
+
+  const course = await Course.findById(id);
+
+  if (!course) {
+    return next(new AppError("Invalid course id or course not found.", 400));
+  }
+
+  // Run only if user sends a file
+  if (req.file) {
+    try {
+      const result = await cloudinary.v2.uploader.upload(req.file.path, {
+        folder: "lms", // Save files in a folder named lms
+        chunk_size: 50000000, // 50 mb size
+        resource_type: "video",
+      });
+
+      // If success
+      if (result) {
+        // Set the public_id and secure_url in array
+        lectureData.public_id = result.public_id;
+        lectureData.secure_url = result.secure_url;
+      }
+
+      // After successful upload remove the file from local storage
+      fs.rm(`uploads/${req.file.filename}`);
+    } catch (error) {
+      // Empty the uploads directory without deleting the uploads directory
+      for (const file of await fs.readdir("uploads/")) {
+        await fs.unlink(path.join("uploads/", file));
+      }
+
+      // Send the error message
+      return next(
+        new AppError(
+          JSON.stringify(error) || "File not uploaded, please try again",
+          400
+        )
+      );
+    }
+  }
+
+  course.lectures.push({
+    title,
+    description,
+    lecture: lectureData,
+  });
+
+  course.numberOfLectures = course.lectures.length;
+
+  // Save the course object
+  await course.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Course Lecture added Successfully",
+    course,
+  });
+});
+
+const removeLectureFromCourse = asyncHandler(async (req, res, next) => {
+  // Grabbing the courseId and lectureId from req.query
+  const { courseId, lectureId } = req.query;
+
+  console.log(courseId);
+
+  // Checking if both courseId and lectureId are present
+  if (!courseId) {
+    return next(new AppError("Course ID is required", 400));
+  }
+
+  if (!lectureId) {
+    return next(new AppError("Lecture ID is required", 400));
+  }
+
+  // Find the course using the courseId
+  const course = await Course.findById(courseId);
+
+  // If no course send custom message
+  if (!course) {
+    return next(new AppError("Invalid ID or Course does not exist.", 404));
+  }
+
+  // Find the index of the lecture using the lectureId
+  const lectureIndex = course.lectures.findIndex(
+    (lecture) => lecture._id.toString() === lectureId.toString()
+  );
+
+  // If returned index is -1 then send error as mentioned below
+  if (lectureIndex === -1) {
+    return next(new AppError("Lecture does not exist.", 404));
+  }
+
+  // Delete the lecture from cloudinary
+  await cloudinary.v2.uploader.destroy(
+    course.lectures[lectureIndex].lecture.public_id,
+    {
+      resource_type: "video",
+    }
+  );
+
+  // Remove the lecture from the array
+  course.lectures.splice(lectureIndex, 1);
+
+  // update the number of lectures based on lectres array length
+  course.numberOfLectures = course.lectures.length;
+
+  // Save the course object
+  await course.save();
+
+  // Return response
+  res.status(200).json({
+    success: true,
+    message: "Course Lecture removed Successfully",
+  });
+});
 
 export {
   getAllCourses,
@@ -232,4 +290,5 @@ export {
   updateCourse,
   removeCourse,
   addLecturesToCourseById,
+  removeLectureFromCourse,
 };
--- routes/course.routes.js
+++ routes/course.routes.js
@@ -5,6 +5,7 @@ import {
   getAllCourses,
   getLecturesByCourseId,
   removeCourse,
+  removeLectureFromCourse,
   updateCourse,
 } from "../controllers/course.controller.js";
 import {
@@ -27,18 +28,18 @@ router
     authorizedRoles("ADMIN"),
     upload.single("thumbnail"),
     createCourse
-  );
+  )
+  .delete(isLoggedIn, authorizedRoles("ADMIN"), removeLectureFromCourse);
 
 router
   .route("/:id")
-  .get(isLoggedIn, authorizeSubscribers, getLecturesByCourseId)
-  .put(isLoggedIn, authorizedRoles("ADMIN"), updateCourse)
-  .delete(isLoggedIn, authorizedRoles("ADMIN"), removeCourse)
+  .get(isLoggedIn, authorizeSubscribers, getLecturesByCourseId) // Added authorizeSubscribers to check if user is admin or subscribed if not then forbid the access to the lectures
   .post(
     isLoggedIn,
     authorizedRoles("ADMIN"),
     upload.single("lecture"),
     addLecturesToCourseById
-  );
+  )
+  .put(isLoggedIn, authorizedRoles("ADMIN"), updateCourse);
 
 export default router;
